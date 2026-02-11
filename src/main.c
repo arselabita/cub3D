e49e20574eea2a67783310345c298b067cc54b08
Arsela Bita <arselabita@gmail.com>
2026-02-11 21:10:21 +0100

    added the check for the start and end of a row

--- src/main.c
+++ src/main.c
@@ -6,11 +6,30 @@
 /*   By: abita <abita@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/05 15:54:05 by abita             #+#    #+#             */
-/*   Updated: 2026/02/11 20:44:12 by abita            ###   ########.fr       */
+/*   Updated: 2026/02/11 21:09:43 by abita            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub.h"
+
+int is_valid_row(char *line)
+{
+	int i;
+	int len;
+
+	if (!line && line[0] == '\0')
+		return (0);
+	i = 0;
+	len = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	len = i;
+	if (line[0] != '1')
+		return (0);
+	if (line[len - 1] != '1')
+		return (0);
+	return (1);
+}
 
 int is_all_ones(char *last_map_line)
 {
@@ -32,8 +51,8 @@ int open_file(void)
 	char    	*next_line;
 	char		*first_map_line;
 	char		*last_map_line;
-	int			line_count;
-	
+	int			is_first_line;
+
 	fd = open("map.cub", O_RDONLY);
 	if (fd == -1)
 		return (print_error("Error: opening the file\n"), ERROR_FD);
@@ -42,7 +61,7 @@ int open_file(void)
 	
 	first_map_line = NULL;
 	last_map_line = NULL;
-	line_count = 0;
+	is_first_line = 1;
 	
 	while ((next_line = get_next_line(fd)) != NULL)
 	{
@@ -64,11 +83,17 @@ int open_file(void)
 		////////////////
 		if (next_line[i] != '\0')
 		{
-			if (first_map_line == NULL)
+			if (is_first_line)
+			{
 				first_map_line = ft_strdup(&next_line[i]);
+				is_first_line = 0;
+			}
 			free(last_map_line);
 			last_map_line = ft_strdup(&next_line[i]);
-			line_count++;
+			if (!is_valid_row(&next_line[i]))
+				return (print_error("ERROR: Row must start and end with 1\n"),
+					free(first_map_line), free(last_map_line), free(next_line), 
+					close(fd), EXIT_FAILURE);	
 		}
 		printf("%s\n", next_line);
 		free(next_line);
